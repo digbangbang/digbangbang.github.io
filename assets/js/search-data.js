4d64cce8c367a67c3cbd05e8d184632de2320e25
digbangbang <78746384+digbangbang@users.noreply.github.com>
2025-03-14 11:58:19 +0000

    Deploying to gh-pages from @ digbangbang/digbangbang.github.io@74e7ead7bcff25177989a302fb335b9d6b3225fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -380,16 +380,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-lpt-fl-is-accepted-by-neurips-2024",
+          title: 'Our paper LPT-FL is accepted by NeurIPS 2024!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
